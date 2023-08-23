87b881e843008d6e82f6b318ad03e88061a3bf62
Javier Caballero <caballeroperezjavier@gmail.com>
2023-08-23 13:25:39 -0500

    Haciendo apartado de rolls

--- src/components/Tiradas/tiradas.jsx
+++ src/components/Tiradas/tiradas.jsx
@@ -1,51 +1,75 @@
-import React from "react";
-import disciplinesData from "../../assets/files/disciplines.JSON";
-import { Nav, NavItem, NavLink, TabContent, TabPane, Card, CardBody, CardTitle, CardText } from "reactstrap";
-import classnames from "classnames";
+import React, { useState } from "react";
+import disciplinesData from "./disciplines.json";
 
-const Disciplines = () => {
-  const [activeTab, setActiveTab] = React.useState("1");
+const DisciplinesComponent = () => {
+  const [selectedDiscipline, setSelectedDiscipline] = useState(null);
 
-  const toggleTab = (tab) => {
-    if (activeTab !== tab) {
-      setActiveTab(tab);
-    }
+  const handleDisciplineClick = (index) => {
+    setSelectedDiscipline(index);
   };
 
   return (
-    <div className="disciplines-container">
-      <Nav tabs>
-        {disciplinesData.disciplines.map((discipline, index) => (
-          <NavItem key={index}>
-            <NavLink
-              className={classnames({ active: activeTab === `${index + 1}` })}
-              onClick={() => toggleTab(`${index + 1}`)}
-            >
-              {discipline.name}
-            </NavLink>
-          </NavItem>
-        ))}
-      </Nav>
-      <TabContent activeTab={activeTab}>
-        {disciplinesData.disciplines.map((discipline, index) => (
-          <TabPane key={index} tabId={`${index + 1}`}>
-            <div className="discipline-card-container">
-              {discipline.levels.map((level, levelIndex) => (
-                <Card key={levelIndex} className="discipline-card">
-                  <CardBody>
-                    <CardTitle tag="h5">{level.name}</CardTitle>
-                    <CardText>
-                      <strong>Roll:</strong> {level.roll}
-                    </CardText>
-                  </CardBody>
-                </Card>
+    <div className="mt-5 bg-black">
+      <div className="row">
+        <div className="col-md-4">
+          <nav>
+            <h2 className="text-white">Disciplines</h2>
+            <ul className="list-group">
+              {disciplinesData.disciplines.map((discipline, index) => (
+                <li
+                  key={index}
+                  className={`list-group-item ${
+                    selectedDiscipline === index ? "active" : ""
+                  }`}
+                  onClick={() => handleDisciplineClick(index)}
+                >
+                  {discipline.name}
+                </li>
               ))}
-            </div>
-          </TabPane>
-        ))}
-      </TabContent>
+            </ul>
+          </nav>
+        </div>
+        <div className="col-md-8">
+          <div>
+            <h1 className="text-white">
+              {selectedDiscipline !== null
+                ? disciplinesData.disciplines[selectedDiscipline].name
+                : "Select a Discipline"}
+            </h1>
+            <p className="text-white">
+              {selectedDiscipline !== null
+                ? disciplinesData.disciplines[selectedDiscipline].description
+                : "Select a Discipline to see its description"}
+            </p>
+          </div>
+          <div className="row">
+            {selectedDiscipline !== null &&
+              disciplinesData.disciplines[selectedDiscipline].abilities.map(
+                (ability, abilityIndex) => (
+                  <div key={abilityIndex} className="col-md-6 mb-4">
+                    <div className="card">
+                      <div className="card-body">
+                        <h5 className="card-title">{ability.power}</h5>
+                        <p className="card-text">{ability.description}</p>
+                        <p className="card-text">
+                          <strong>Level:</strong> {ability.level}
+                        </p>
+                        <p className="card-text">
+                          <strong>Cost:</strong> {ability.cost}
+                        </p>
+                        <p className="card-text">
+                          <strong>Reference:</strong> {ability.reference}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                )
+              )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Disciplines;
+export default DisciplinesComponent;
